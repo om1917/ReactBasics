a5bbe4952a300254bcb233ce8b84b175ef9288ae
Om <om.bhaisora@gmail.com>
2023-12-04 00:02:34 +0530

    Addon

---
+++ context/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import UserContext from "../context/UserContext";
+import { useContext } from "react";
+
+export default function Profile() {
+    const { user } = useContext(UserContext)
+    if (!user) return <div>Please Login !</div>
+    return <div>Welcome {user.userName}</div>
+}
---
+++ reactRouter/src/components/GitHub/GitHub.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+function GitHub(){
+    const [data,setData] = useState([]); 
+
+    useEffect(()=>{
+        fetch('https://api.github.com/users/om1917')
+        .then(res=>res.json())
+        .then(data=>{
+            setData(data);
+        });
+    },[])
+    return (
+        <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+            GitHub Followers : {data.followers}
+            <img src={data.avatar_url} alt="GitHub Profile" width={300}/>
+        </div>
+    );
+}
+
+export default GitHub;
+
+export const gitHubInfoLoader = async()=>{
+   const res = await fetch('https://api.github.com/users/om1917');
+   return res.json()
+}
